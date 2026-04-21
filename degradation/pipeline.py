5128e2e9669ef3bd30845a01176245ad2f4fc841
SONU SUDHIKUMAR SEENA <60896516+sonusudhi@users.noreply.github.com>
2026-04-21 21:34:09 +0100

    Reorder authors in pipeline.py

--- degradation/pipeline.py
+++ degradation/pipeline.py
@@ -17,7 +17,7 @@ Pipeline order:
     8. Ring artefacts disabled (absent at 25 mm FOV)
 
 Authors:
-    Sonu Sudhikumar Seena (1,2), Lin Ma (1), Anirban Chakraborty (2), Jingyue Hao (1)
+    Sonu Sudhikumar Seena (1), Anirban Chakraborty (2), Jingyue Hao (1), Lin Ma (1)
 
 Implementation:
     Sonu Sudhikuamr Seena
