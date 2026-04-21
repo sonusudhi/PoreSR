8466e0a715440866dc2533500ad0c911b5e08109
SONU SUDHIKUMAR SEENA <60896516+sonusudhi@users.noreply.github.com>
2026-04-21 21:33:25 +0100

    Reorder authors in generator.py comments

--- models/generator.py
+++ models/generator.py
@@ -5,7 +5,7 @@ Modified SRResNet with optional CBAM attention for 2D and 2.5D
 super-resolution of rock micro-CT images.
 
 Authors:
-    Sonu Sudhikumar Seena (1,2), Lin Ma (1), Anirban Chakraborty (2), Jingyue Hao (1)
+    Sonu Sudhikumar Seena (1), Anirban Chakraborty (2), Jingyue Hao (1), Lin Ma (1)
 
 Implementation:
     Sonu Sudhikumar Seena
