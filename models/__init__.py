e93b12dbee4f62efc791911d4c4397946b85eacb
SONU SUDHIKUMAR SEENA <60896516+sonusudhi@users.noreply.github.com>
2026-09-05 22:27:39 +0100

    Add files via upload

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,23 @@
-from .generator import SRResNet, CBAM, ResidualBlock, UpsampleBlock
+"""Generator and discriminator architectures."""
+
+from .generator import (
+    CBAM,
+    GENERATOR_CONFIGS,
+    ResidualBlock,
+    SRResNet,
+    UpsampleBlock,
+    build_generator,
+    count_parameters,
+)
 from .discriminator import PatchDiscriminator
+
+__all__ = [
+    "CBAM",
+    "GENERATOR_CONFIGS",
+    "PatchDiscriminator",
+    "ResidualBlock",
+    "SRResNet",
+    "UpsampleBlock",
+    "build_generator",
+    "count_parameters",
+]
